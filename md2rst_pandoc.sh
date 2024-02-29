9556534418d85493d74eba8d5f5f6e11c737d373
Dongliang Mu <dzm91@hust.edu.cn>
2024-02-29 08:33:04 +0800

    add pandoc converter from markdown to rst

---
+++ md2rst_pandoc.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ $# -ne 1 ]; then
+	echo "Please provide a file with Markdown type as the 1st parameter!"
+	exit 0
+fi
+
+TARGET=/tmp/target.rst
+
+pandoc $1 -f markdown -t rst -o ${TARGET}
+
+if [ $? == 0 ];then
+	echo "The pandoc convertion is successful! Please check $TARGET!"
+else
+	echo "The pandoc covertion fails!"
+fi
